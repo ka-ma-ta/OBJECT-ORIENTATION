4b663f9fc6d2037817631e3df4a1d318ff1506a4
kamada taro <wankoroy123wan@gmail.com>
2023-02-06 22:20:55 +0900

    first commit

---
+++ question01.php
@@ -0,0 +1,52 @@
+<?php  
+//デバッグ環境構築する！！！
+
+//Carクラス
+class Car {
+
+    private $name;
+    private $capacity;
+    private $price;
+    private $speed;
+
+    //アクセル
+    function accelerator(){
+        //
+    }
+
+    //ブレーキ
+    function brake(){
+        //
+    }
+
+    //結果を出力する関数
+    function __construct($name, $capacity, $price, $speed){
+        //改行する、シングルクォーテーションorダブルクォーテーションに注意
+        echo "車種名：", $name, "\n";
+        echo "--定員：", $capacity, "人\n";
+        echo "--価格：", $price, "万円\n";
+        echo "--加速：", $speed, "m/s\n";
+        echo "\n";
+        echo "--アクセル：あり\n";
+        echo "--ブレーキ：あり\n";
+        echo "\n";
+        echo "\n";
+    }
+
+}
+
+//問題文を満たすように、任意の数字を渡す
+
+//Honda
+$Honda = new Car('honda', 7, 150, 20,);
+
+//Nissan
+$Nissan = new Car('nissan', 7, 100, 20);
+
+//Ferrari
+$Ferrari = new Car('ferrari', 2, 300, 50);
+
+//デバッグ環境構築　→　テスト＆デバッグ実行
+
+
+?>
