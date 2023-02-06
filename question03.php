f99a415cec345aa074a7b9fc0168c7069672c374
kamada taro <wankoroy123wan@gmail.com>
2023-02-07 00:35:29 +0900

    5回目

--- question03.php
+++ question03.php
@@ -30,12 +30,31 @@ $Ferrari = new Car('ferrari', 2, 300, 50);
 */
 
 //関数：ランダム数の作成（台数）
-
-
-//関数：ランダム数の作成（価格）
-
-
+function Number(){
+    //1~20のランダムな整数を作成
+    $random01 = rand(1, 20);
+    //作成した数値を返す
+    return $random01;
+    //
+}
 //
+//関数：ランダム数の作成（価格）
+function Price(){
+    //100~500のランダムな整数を作成
+    $random02 = rand(100, 500);
+    //作成した数値を返す
+    return $random02;
+    //
+}
+//
+//Honda作成
+$max = Number();
+$subprice = Price();
+For($i=0; $i<$max; $i++){
+    //
+    $Honda = new Car('honda', 7, $subprice, 20,);
+    //
+}
 
 
 ?>
