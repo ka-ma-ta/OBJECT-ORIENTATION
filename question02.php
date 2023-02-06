0bcd8d1850802bb7f853106cfb166129e8c77814
kamada taro <wankoroy123wan@gmail.com>
2023-02-07 00:09:45 +0900

    ADD : question02.php , question03.php

---
+++ question02.php
@@ -0,0 +1,37 @@
+<?php 
+
+//Carクラス
+class Car {
+
+    private $name;
+    private $capacity;
+    private $price;
+    private $speed;
+
+    //アクセル
+    function accelerator(){
+        //
+    }
+
+    //ブレーキ
+    function brake(){
+        //
+    }
+
+}
+
+//Ferrariクラス
+class Ferrari extends Car(){
+    //情報
+    $this->name
+
+    //リフトダウン
+
+
+    //リフトアップ
+
+
+}
+
+
+?>
---
+++ question03.php
@@ -0,0 +1,41 @@
+<?php  
+
+//Carクラス
+class Car {
+
+    private $name;
+    private $capacity;
+    private $price;
+    private $speed;
+
+    //アクセル
+    function accelerator(){
+        //
+    }
+
+    //ブレーキ
+    function brake(){
+        //
+    }
+
+}
+
+/*
+//Honda
+$Honda = new Car('honda', 7, 150, 20,);
+//Nissan
+$Nissan = new Car('nissan', 7, 100, 20);
+//Ferrari
+$Ferrari = new Car('ferrari', 2, 300, 50);
+*/
+
+//関数：ランダム数の作成（台数）
+
+
+//関数：ランダム数の作成（価格）
+
+
+//
+
+
+?>
