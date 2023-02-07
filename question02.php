4f6d0a121adc017f745e04b7297cc943013550ea
kamada taro <wankoroy123wan@gmail.com>
2023-02-08 00:17:37 +0900

    7回目

--- question02.php
+++ question02.php
@@ -1,39 +1,103 @@
 <?php 
-
+//
 //Carクラス
 class Car {
-
-    private $name;
-    private $capacity;
-    private $price;
-    private $speed;
-
+    //
+    public $name;
+    public $capacity;
+    public $price;
+    public $speed;
+    //
     //アクセル
     function accelerator(){
         //
     }
-
+    //
     //ブレーキ
     function brake(){
         //
     }
-
+    //
+    //結果を出力する関数
+    function __construct($name, $capacity, $price, $speed){
+        echo "～初期値～", "\n";
+        echo "車種名：", $name, "\n";
+        echo "--定員：", $capacity, "人\n";
+        echo "--価格：", $price, "万円\n";
+        echo "--加速：", $speed, "m/s\n";
+        echo "\n";
+        echo "--アクセル：あり\n";
+        echo "--ブレーキ：あり\n";
+        echo "\n";
+    }
+    //
 }
-
+//
 //Ferrariクラス
 class Ferrari extends Car{
-    //情報
-    
-
-    //リフトダウン
-
-
-    //リフトアップ
-
+    //
+    //関数：出力
+    function Output($result, $r_Height, $r_Speed){
+        echo $result, "\n";
+        echo "--車高：", $r_Height, "mm\n";
+        echo "--加速：", $r_Speed, "mm\n";
+    }
+    //
+    //関数：リフトアップ or リフトダウン
+    function ChangeHeight($subSpeed){
+        //車高：標準は0
+        $height = 0;
+        //
+        //ループ：5回
+        For($i=1; $i<=5; $i++){
+            //回数出す
+            echo $i,"回目\n";
+            //ランダム値の作成
+            $random = rand(1, 2);
+            //
+            //1→リフトアップ　2→リフトダウン
+            //比較演算子に注意!!
+            if($random==1){
+                //リフトアップ
+                if($height==0){
+                    $height=40;
+                    $subSpeed = $subSpeed*0.8;
+                    echo "リフトアップ出来ました。", "\n";
+                    echo "--車高：", $height, "mm\n";
+                    echo "--加速：", $subSpeed, "m/s\n";
+                }else{
+                    echo "リフトアップ出来ませんでした。", "\n";
+                    echo "--車高：", $height, "mm\n";
+                    echo "--加速：", $subSpeed, "m/s\n";
+                }
+                //
+            }else{
+                //リフトダウン
+                if($height==40){
+                    $height=0;
+                    $subSpeed = $subSpeed/0.8;
+                    echo "リフトダウン出来ました。", "\n";
+                    echo "--車高：", $height, "mm\n";
+                    echo "--加速：", $subSpeed, "m/s\n";
+                }else{
+                    echo "リフトダウン出来ませんでした。", "\n";
+                    echo "--車高：", $height, "mm\n";
+                    echo "--加速：", $subSpeed, "m/s\n";
+                }
+                //
+            }  
+            //改行
+            echo "\n";
+            //
+        }
+        //
+    }
+    //
 }
-
-
-
-
-
+//
+$Ferrari = new Ferrari('ferrari', 2, 300, 50);
+$Ferrari->ChangeHeight(50);  //ここ！！
+//
+//処理まとめる、変数の呼び出し見直す
+//
 ?>
--- question03.php
+++ question03.php
@@ -1,60 +1,81 @@
 <?php  
-
+//
 //Carクラス
 class Car {
-
+    //
     private $name;
     private $capacity;
     private $price;
     private $speed;
-
+    //
     //アクセル
     function accelerator(){
         //
     }
-
+    //
     //ブレーキ
     function brake(){
         //
     }
-
-}
-
-/*
-//Honda
-$Honda = new Car('honda', 7, 150, 20,);
-//Nissan
-$Nissan = new Car('nissan', 7, 100, 20);
-//Ferrari
-$Ferrari = new Car('ferrari', 2, 300, 50);
-*/
-
-//関数：ランダム数の作成（台数）
-function Number(){
-    //1~20のランダムな整数を作成
-    $random01 = rand(1, 20);
-    //作成した数値を返す
-    return $random01;
+    //
+    //結果を出力する関数
+    function __construct($name, $capacity, $price, $speed){
+        //改行する、シングルクォーテーションorダブルクォーテーションに注意
+        echo "車種名：", $name, "\n";
+        echo "--定員：", $capacity, "人\n";
+        echo "--価格：", $price, "万円\n";
+        echo "--加速：", $speed, "m/s\n";
+        echo "\n";
+        echo "--アクセル：あり\n";
+        echo "--ブレーキ：あり\n";
+        //echo "\n";
+        echo "\n";
+        //
+    }
+    //
+    function Calculation($num, $amount){
+        //
+        //計算
+        $total = $num*$amount;
+        //
+        //配列で返す(合計金額, 台数)
+        return array($total, $num);
+        //
+    }
     //
 }
 //
-//関数：ランダム数の作成（価格）
-function Price(){
-    //100~500のランダムな整数を作成
-    $random02 = rand(100, 500);
-    //作成した数値を返す
-    return $random02;
+$CarModel = [
+    ['honda', 7, rand(251, 350), 20],  //Honda　普通
+    ['nissan', 7, rand(100, 250), 20],  //Nissan　安い
+    ['ferrari', 2, rand(351, 500), 50]  //Ferrari　高い
+];
+//
+$arrNum = [];
+$arrTotal = [];
+//
+//ループ：車の種類数繰り返す
+for($i=0; $i<count($CarModel); $i++){
+    //インスタンス作成、基本情報表示
+    $SubCar = new Car($CarModel[$i][0], $CarModel[$i][1], $CarModel[$i][2], $CarModel[$i][3]);
+    //
+    //計算、生産台数、金額を表示
+    $result = $SubCar->Calculation(rand(10, 50), $CarModel[$i][2]);  //台数：10~50台
+    //
+    echo "--生産台数：", $result[1], "台\n";
+    echo "--金額：", $result[0], "万円\n";
+    echo "\n";
+    echo "\n";
+    echo "\n";
+    //
+    //配列に追加しておく
+    $arrNum[] = $result[1];
+    $arrTotal[] = $result[0];
     //
 }
 //
-//Honda作成
-$max = Number();
-$subprice = Price();
-For($i=0; $i<$max; $i++){
-    //
-    $Honda = new Car('honda', 7, $subprice, 20,);
-    //
-}
-
-
+//計算
+echo "合計金額：", array_sum($arrTotal), "万円\n";
+echo "平均金額：", array_sum($arrTotal)/array_sum($arrNum), "万円\n";
+//
 ?>
